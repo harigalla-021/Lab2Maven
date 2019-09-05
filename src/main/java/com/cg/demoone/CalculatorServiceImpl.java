c9d036ed5fcdbce338dd4614fb6e557c150c680c
unknown <hgalla@PUNHWD12790.corp.capgemini.com>
2019-09-05 16:52:52 +0530

    first

---
+++ src/main/java/com/cg/demoone/CalculatorServiceImpl.java
@@ -0,0 +1,26 @@
+package com.cg.demoone;
+
+
+public class CalculatorServiceImpl implements ICalculatorService	{
+
+	public double addition(double numOne, double numTwo) {
+		// TODO Auto-generated method stub
+		return numOne+numTwo;
+	}
+
+	public double subTraction(double numOne, double numTwo) {
+		// TODO Auto-generated method stub
+		return numTwo-numOne;
+	}
+
+	public double mulTiplication(double numOne, double numTwo) {
+		// TODO Auto-generated method stub
+		return numOne*numTwo;
+	}
+
+	public double division(double numOne, double numTwo) {
+		// TODO Auto-generated method stub
+		return numOne/numTwo;
+	}
+
+}
